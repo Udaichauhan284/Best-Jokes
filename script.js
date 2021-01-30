2b1649f44098a7c9d09c521b200ad0f0d6a8dcf9
Udai Chauhan <udaichauhan284@gmail.com>
2021-01-30 19:35:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+//promise and fetch
+
+const joke=document.querySelector('#joke');
+const jokeBtn=document.querySelector('#jokeBtn');
+
+// const generateJokes = () =>{
+//     const setHeader = {
+//         headers : {
+//             Accept : "application/json"
+//         }
+//     }
+
+//     fetch('https://icanhazdadjoke.com',setHeader)
+//     .then((res)=>res.json())
+//     .then((data) => {
+//         joke.innerHTML=data.joke;
+//     }).catch((error) =>{
+//         console.log(error);
+//     })
+// }
+
+//Async Await
+const generateJokes = async () => {
+    try{
+        const setHeader = {
+            headers: {
+                Accept : "application/json"
+            }
+        }
+        const res= await fetch('https://icanhazdadjoke.com',setHeader);
+        const data= await res.json();
+        joke.innerHTML=data.joke;
+    }catch(error){
+        console.log(`this is a ${error}`);
+    }
+}
+jokeBtn.addEventListener('click',generateJokes);
+generateJokes();
